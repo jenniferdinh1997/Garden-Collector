b8625da27dd0faf828fa8dddf3c3de05235bb156
jenniferdinh1997 <jenniferdinh1997@gmail.com>
2022-04-25 14:42:59 -0700

    about view

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,5 @@ from . import views #referring to the views file
 
 urlpatterns = [ #defines a root path using an empty string and maps it to the view.home function (name='home') references the URL in other parts of the app
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
 ]
