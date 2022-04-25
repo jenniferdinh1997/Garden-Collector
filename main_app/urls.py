2eccc7dea1068f8ac4754a9d87d309771d800ca3
jenniferdinh1997 <jenniferdinh1997@gmail.com>
2022-04-25 14:38:12 -0700

    add views

---
+++ main_app/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views #referring to the views file
+
+urlpatterns = [ #defines a root path using an empty string and maps it to the view.home function (name='home') references the URL in other parts of the app
+    path('', views.home, name='home'),
+]
